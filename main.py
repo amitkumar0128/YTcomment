30eca37672cbf371b9575a4724f0a4e84dbd569a
amitkumar_0128 <jangidamitkumar49@gmail.com>
2025-07-06 19:45:03 +0530

    write data to a file

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main(url=None, sort_by=SORT_BY_POPULAR, max_comments=10):
         counter += 1
     return output
 
+def write_to_file(data, filename='comments.json'):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=2)
+
 if __name__ == "__main__":
     try:
         # url = input("Enter the YouTube video URL: ")
@@ -19,8 +23,9 @@ if __name__ == "__main__":
         sort_by = SORT_BY_POPULAR  # or SORT_BY_NEWEST, SORT_BY_OLDEST
         max_comments = 10  # Set the maximum number of comments to retrieve
         if url:
-            output = main(url, sort_by, max_comments)
-            print(json.dumps(output, indent=2))  # Use json.dumps to ensure proper JSON formatting
+            data = main(url, sort_by, max_comments)
+            print(json.dumps(data, indent=2))  # Use json.dumps to ensure proper JSON formatting
+            write_to_file(data)
         else:
             print("No URL provided. Exiting.")
     except Exception as e:
